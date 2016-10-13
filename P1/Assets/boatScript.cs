7654ff0a3079fcb5a88a412a05e5f2f0caadfb59
alu0100818369 <Natalie@users-MacBook-Pro-251.local>
2016-10-13 14:30:25 +0100

    Añadiendo ficheros
    Al tocar la barca esta desaparece y aparecen unas esferas (burbujas)

---
+++ P1/Assets/boatScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public delegate void metodoDelegao ();
+
+public class boatScript : MonoBehaviour {
+	public event metodoDelegao Evento;
+
+	void OnTriggerEnter(Collider other) {
+		Evento ();
+		this.gameObject.SetActive (false);
+	}
+}
